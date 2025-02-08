9185f29c28ee7b765801ee17dd979409659f1746
Denis <mail@gmail.com>
2025-02-08 15:36:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.clicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer count = 10000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+
+    public void onClickButton(View view) {
+        count++;
+        TextView txt= findViewById(R.id.txtCounter);
+        txt.setText(count.toString());
+    }
+}
