656f14df2726df9af1f66b91c3d715586d69e347
Denis <mail@gmail.com>
2025-02-14 11:13:54 +0300

    изменено

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.clicker;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,13 +24,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
+        Toast.makeText(this, "onCreate",Toast.LENGTH_LONG).show();
     }
-
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Instens", "onStart");
+        Toast.makeText(this, "onStart",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Instens", "onResume");
+        Toast.makeText(this, "onResume",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("Instens", "onPause");
+        Toast.makeText(this, "onPause",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("Instens", "onStop");
+        Toast.makeText(this, "onStop",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("Instens", "onDestroy");
+        Toast.makeText(this, "onDestroy",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle autState){
+        super.onSaveInstanceState(autState);
+        autState.putInt("Click", count);
+        Log.d("Instens", "onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle SavedState){
+        super.onRestoreInstanceState(SavedState);
+        if(SavedState != null && SavedState.containsKey("Click")){
+            count = SavedState.getInt("Click");
+        }
+        Log.d("Instens","onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_LONG).show();
+    }
     public void onClickButton(View view) {
         count++;
-        TextView txt= findViewById(R.id.txtCounter);
+        TextView txt = findViewById(R.id.txtCounter);
+        txt.setText(count.toString());
+    }
+    public void onClickButton2(View view){
+        count = 0;
+        TextView txt=findViewById(R.id.txtCounter);
         txt.setText(count.toString());
     }
 }
